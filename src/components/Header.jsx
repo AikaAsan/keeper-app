2b07b484a8e4c3c09cf93f718073147949676ebd
Aika <aiganysha@gmail.com>
2021-01-14 14:02:32 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,15 @@ function Header() {
         <header>
             
             <h1>
-                <HighlightIcon /> NoteKeeper</h1>
+                <HighlightIcon /> 
+                NoteKeeper
+                
+            </h1>
         </header>
     );
 }
 
 export default Header;
+
+
+
