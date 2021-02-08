e5f12e1ff53be151d2f6073644788471e38d437f
aika <aiganysha@gmail.com>
2021-02-08 13:08:53 -0600

    delete commnets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,36 +8,6 @@ import axios from "axios";
 
 
 function App() {
-    // const [notes, setNotes] = useState([]);
-
-    // function addNote(note) {
-    //     setNotes(prevNotes => {
-    //         return [...prevNotes, note];
-    //     });
-
-    // }
-    // function deleteNote(id) {
-    //     console.debug('id:', id)
-    //     axios.delete('http://localhost:5000/' + id)
-    //         .then(res => console.log(res.data));
-
-    //     setNotes(prevNotes => {
-    //         return prevNotes.filter(note => {
-    //             return note._id !== id;
-    //         });
-    //     });
-    // }
-
-    // function displayNotes(note) {
-    //     return <Note key={note._id} title={note.title} content={note.content} />
-    // }
-    // function deleteNote(id) {
-    //     setNotes(prevNotes => {
-    //         return prevNotes.filter((note, index) => {
-    //             return index !== id;
-    //         });
-    //     });
-    // }
 
     return (
         <Router>
